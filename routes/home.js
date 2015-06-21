2759ca4fc7155b85bd79c2507f0ce8430d36131b
dsxbb <13192872690@163.com>
2015-06-21 11:45:48 +0800

    后端增加文件上传、重命名文件，读取文件命令功能

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+/**
+ * 起始路由
+ */
 router.get('/home',function(req, res, next){
 	res.render('index');
+	next();
+});
+
+/**
+ * 获取某个类型的所有文件或目录
+ */
+router.get('/category/:type', function(res, req, next) {
+	next();
+})
+
+/**
+ * 创建新目录
+ */
+router.post('/api/create', function(res, req, next) {
+	next();
+});
+
+/**
+ * 重命名文件目录
+ */
+router.post('/api/rename', function(res, req, next) {
+	next();
+});
+
+/**
+ * 删除某个文件或目录
+ */
+router.post('/api/delete', function(res, req, next) {
 	next();
 });
 
